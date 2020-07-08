fe91d86ebdce1d177f4a48b4fe193b92c6282366
Dong Huu Cao <43071789+dcaohuu18@users.noreply.github.com>
2020-07-08 10:41:42 -0400

    add Bokeh histogram for grade distribution

--- c_stats.py
+++ c_stats.py
@@ -1,17 +1,26 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
 from datetime import datetime
 from data.read_data import read_last_time, read_n_recent
+from helper_functions import graph_hist, estimate_pdf
 
 
 MONTH_SEM_MAP = {mon: 'FA' if 8<=mon<=12 else 'SP' for mon in range(1, 13)} 
 # assume there're only 2 semesters: Fall (FA) and Spring (SP)
 
 
-def basics(course, current_term):
-	st.header('Basics')
+def basics(course, current_term, cur_course_df):
+    st.header('Basics')
+
+    grades_df = cur_course_df.iloc[:, 8:-9]
+    grades_df.dropna(how='all', axis=1, inplace=True) # drop unavailable grades
+
+    hist_layout = graph_hist(grades_df, 'Distribution of grades', 'Grades', 'Number of students')
+    st.bokeh_chart(hist_layout)
 
 def advanced(course, current_term):
-	st.header('Advanced')
+        st.header('Advanced')
 
 def main():
     # Title 
@@ -28,15 +37,19 @@ def main():
     cur_course_df = read_n_recent(course, current_term)
     # st.dataframe(cur_course_df)
 
-	# Functionality options:
+    # Functionality options:
     func_options = ['Basics', 'Advanced']
     func_choice = st.sidebar.radio('Options', func_options)
 
     if func_choice == 'Basics':
-	    basics(course, current_term)
+        basics(course, current_term, cur_course_df)
 
     elif func_choice == 'Advanced':
         advanced(course, current_term)
+
+    # About:
+    st.sidebar.header('About')
+    st.sidebar.info('C_STATS is a learning management and analytics tool.')
 
 
 if __name__ == '__main__':
